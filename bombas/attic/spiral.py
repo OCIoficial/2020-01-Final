f8d0439a60006477e17db4829587cb3130142198
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2020-01-08 23:07:41 -0300

    More test cases

--- bombas/attic/spiral.py
+++ bombas/attic/spiral.py
@@ -1,15 +1,22 @@
+import queue
 import random
 import sys
 
 m = int(sys.argv[2])
 n = int(sys.argv[3])
-s = int(sys.argv[4])
+pattern = sys.argv[4]
 
-k = 0
+bombs = []
 
-print(m, n)
+grid = [[1e6 for i in range(n + 2)] for j in range(m + 2)]
 
-bombs = ""
+for i in range(m + 2):
+  grid[i][0] = 0
+  grid[i][n + 1] = 0
+
+for j in range(n + 2):
+  grid[0][j] = 0
+  grid[m + 1][j] = 0
 
 mi = 1
 mf = m
@@ -22,9 +29,10 @@ while True:
     if mf - mi <= 3:
       break
     for j in range(0, nf - ni):
-      bombs += "{} {} {}\n".format(1, mi + 1, nf - j)
-      bombs += "{} {} {}\n".format(1, mf - 1, ni + j)
-      k += 2
+      bombs.append((1, mi + 1, ni + j))
+      bombs.append((1, mf - 1, nf - j))
+      grid[mi + 1][ni + j] = 0
+      grid[mf - 1][nf - j] = 0
     mi += 2
     mf -= 2
     o = not o
@@ -32,18 +40,47 @@ while True:
     if nf - ni <= 3:
       break
     for i in range(0, mf - mi):
-      bombs += "{} {} {}\n".format(1, mi + i, ni + 1)
-      bombs += "{} {} {}\n".format(1, mf - i, nf - 1)
-      k += 2
+      bombs.append((1, mi + i, nf - 1))
+      bombs.append((1, mf - i, ni + 1))
+      grid[mi + i][nf - 1] = 0
+      grid[mf - i][ni + 1] = 0
     ni += 2
     nf -= 2
     o = not o
 
-if s:
+
+# block-center: Block center space, will make an unpassable grid
+if pattern == "block-center":
   for i in range((m - 1) // 2, (m + 4) // 2):
     for j in range((n - 1) // 2, (n + 4) // 2):
-      bombs += "{} {} {}\n".format(1, i, j)
-      k += 1
+      bombs.append((1, i, j))
 
-print(k)
-print(bombs[:-1])
+
+# block-past: Block cells just behind the right route
+# block-over: Previous case with a 1 turn offset, will make an unpassable grid
+if pattern in ["block-past", "block-over"]:
+  offset = {
+    "block-past": 1,
+    "block-over": 0
+  }[pattern]
+  bfs = queue.Queue()
+  bfs.put((1, 1, 1))
+  while not bfs.empty():
+    turn, row, col = bfs.get()
+    if grid[row][col] <= turn:
+      continue
+    grid[row][col] = turn
+    bfs.put((turn + 1, row + 1, col))
+    bfs.put((turn + 1, row - 1, col))
+    bfs.put((turn + 1, row, col + 1))
+    bfs.put((turn + 1, row, col - 1))
+
+  for row in range(1, m + 1):
+    for col in range(1, n + 1):
+      if (row, col) != (1, 1) and (row, col) != (m, n) and 0 < grid[row][col] < m * n:
+        bombs.append((grid[row][col] + offset, row, col))
+
+print(m, n)
+print(len(bombs))
+for turn, row, col in bombs:
+  print(turn, row, col)
